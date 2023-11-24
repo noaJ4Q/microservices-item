d18072b7f44d8f18f45141a6802cbb23284c8b35
Noe Jara <noe.jaraq@gmail.com>
2023-11-24 15:29:40 -0500

    first commit

---
+++ src/main/java/com/example/microservicesitem/MicroservicesItemApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicesitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesItemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesItemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/microservicesitem/MicroservicesItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicesitem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesItemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
